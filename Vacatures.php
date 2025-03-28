538db1d036821daf491292dbb52a9fd98df9fbf3
marijnhaas <6046836@mborijnland.nl>
2025-03-28 13:31:58 +0100

    vacatures pagina aangemaakt
    helaas is deze niet af omdat mohammed er bijna nooit is

---
+++ Vacatures.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Vlam en Vlees is een gezellig restaurant met heerlijk eten.">
+    <meta name="keywords" content="Vlees, Restaurant, Vleesrestaurant">
+    <meta name="author" content="Mohammed Bali">
+    <title>Vacatures</title>
+    <link rel="stylesheet" href="CSS/style.css">
+    <script src="JS/hamburger.js"></script>
+</head>
+<body>
+
+    <?php include 'Header/header.php'; ?>
+
+    <main>
+
+    </main>
+
+    <?php include 'Footer/footer.php'; ?>
+
+</body>
+</html>
