8a19ce0841d4c696e31516dd14fdc696af6fd5a9
marijnhaas <6046836@mborijnland.nl>
2025-03-28 13:15:57 +0100

    Update header.php
    nav geupdate

--- Header/header.php
+++ Header/header.php
@@ -4,7 +4,7 @@
         <ul>
             <li><a href="menu.php">Menu</a></li>
             <li><a href="openingstijden.php">Openingstijden</a></li>
-            <li><a href="#">Reserveren</a></li>
+            <li><a href="reservering.php">Reserveren</a></li>
             <li><a href="#">Vacatures</a></li>
         </ul>
     </nav>
@@ -13,7 +13,7 @@
         <a href="index.php">Home</a>
         <a href="menu.php">Menu</a>
         <a href="openingstijden.php">Openingstijden</a>
-        <a href="#">Reserveren</a>
+        <a href="reservering.php">Reserveren</a>
         <a href="#">Vacatures</a>
 </nav>
     <button class="openbtn" onclick="openNav()">&#9776;</button>
